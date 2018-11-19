ed041d7a2fc34fc2a033b9b187916bbea63d53a8
Himashi Hettege Dona <himashi99@hotmail.com>
2018-11-19 06:46:12 -0500

    Clean theme

--- wp-content/themes/three-c/page.php
+++ wp-content/themes/three-c/page.php
@@ -18,19 +18,8 @@ get_header();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
+			<div class="main-container">
+			</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
